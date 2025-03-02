519b6eed42b52643240cb5017126efb7cc0b9ac3
Slothdemon22 <162999948+Slothdemon22@users.noreply.github.com>
2025-03-03 01:34:52 +0500

    Update page.tsx with another another change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org → i changed something
+          Go to nextjs.org → i changed something another another change
         </a>
       </footer>
     </div>
